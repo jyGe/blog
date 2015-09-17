779c21fa3138e8b1a140af85a8e5693c7e8224ee
jyGe <515228740@qq.com>
2015-09-17 16:25:13 +0800

    change a little styles

--- sblog/urls.py
+++ sblog/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	# url(r'^$', views.BlogListView.as_view(), name='bloglist'),
-	url(r'^$', views.listing, name='bloglist'),
+	# url: blog
+	url(r'^blog/$', views.listing, name='bloglist'),
 	url(r'^blog/(?P<pk>[0-9]+)/$', views.BlogDtailView.as_view(), 
 		name='detailblog'),
 	url(r'^add/$', views.add_blog, name='addblog'),
 	url(r'^blog/(?P<blog_id>[0-9]+)/del/$', views.delete_blog, name='delblog'),
+
+	# url: about me
 ]
